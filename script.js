15cd5b970c1846d5cfb0ef3a35f886ef04eabc72
Edun Rilwan <111396734+Omotunde2005@users.noreply.github.com>
2024-07-26 20:09:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// Strings
+var first_name = "Rilwan";
+var last_name = "Edun";
+var favorite_pet = "Dog";
+var nickname = "Emiloju";
+var university = "Unilag";
+
+
+// Numbers
+var age = 19;
+var favorite_number = 7;
+var account_number = 1461333920;
+var house_number = 1;
+var num_of_followers = 106;
+
+
+// Booleans
+var is_handsome = true;
+var is_married = false;
+var is_tech_bro = true;
+var is_tall = true;
+var is_intelligent = true;
+
+
+// Null
+var married = null;
+var single = null;
+var divorced = null;
+var topic = null;
+var post = null;
+
+
+// Undefined
+var animal, color, complexion, height, weight;
